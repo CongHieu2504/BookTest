d22f01f167d82186798ee3dc49cadf8065a05dc9
Cong Hieu <hieuho250403@gmail.com>
2025-10-29 11:00:41 +0700

    Deloy FE

--- src/service/axios.customize.js
+++ src/service/axios.customize.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://bookshop-test-deploy.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json", // Gửi dữ liệu dưới dạng JSON
